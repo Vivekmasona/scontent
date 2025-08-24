9a027d40b3a2e9d3d284029f403b88deeaae9e18
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2025-08-24 23:37:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const PRIORITY_DOMAINS = [
   "vimeo.com"
 ];
 
-app.get("/extract", async (req, res) => {
+app.get("/cdn", async (req, res) => {
   const { url } = req.query;
   if (!url) return res.status(400).json({ error: "Valid URL required" });
 
